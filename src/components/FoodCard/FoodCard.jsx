dce0f586f740b44d8b4d72ede89afa082156c5d8
Karolina Dbaj <k.dbaj@outlook.com>
2023-05-10 11:03:49 +0200

    feat: added tags to FoodCard component

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./FoodCard.css";
+import Tag from "../Tag/Tag";
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+</style>
 
-function FoodCard({image, name, description, buttonText}) {
+function FoodCard({image, name,tags}) {
+
+  
+
     return(
-        <div className="card">
-        <img src={image} alt={name} />
-        <h3 className="title">{name}</h3>
-        <p>{description}</p>
-        <button>{buttonText}</button>
+      <div className="card">
+        <div className="card-image">
+          <div className="image-container">
+            <img src={image} alt={name}/>
+          </div>
+          <div>
+             <h3 className="title">{name} </h3>
+            <div className="tagContainer">
+              {
+                tags.slice(0,3).map((tag) => {
+                  return <Tag tag={tag}/>
+                }
+                )}
+            </div>
+          </div>
+        </div>
       </div>
     )
-    
 }
 
 export default FoodCard;
