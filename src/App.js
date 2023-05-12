866013639df61422fddf793197f3c00e9ca9cf34
Karolina Dbaj <k.dbaj@outlook.com>
2023-05-12 21:34:31 +0200

    added activeTag function

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import "./App.css";
-import FoodCard from "./components/FoodCard/FoodCard";
+import { useState, useEffect } from 'react'
+import './App.css'
+import FoodCard from './components/FoodCard/FoodCard'
+import TagMenu from './components/TagMenu/TagMenu'
 
 const TEMP_DATA = [
   {
-    name: "Makaron",
+    name: 'Makaron',
     image:
-      "https://mediaconvert.kukbuk.pl/ksTNHdwFDa7biY17BBiWpov4JEeRSOx4uNtbqEs6RrM/fill/1418/1682/fp:0.5:0.5/1/aHR0cHM6Ly9tZWRp/YS5rdWtidWsucGwv/bWVkaWEvaW1hZ2Vz/LzIwMTkvMDMtMTgv/ZmV0dHVjaW5lMV90/aW5pZmllZF90aW5p/ZmllZC5qcGc.jpg",
+      'https://mediaconvert.kukbuk.pl/ksTNHdwFDa7biY17BBiWpov4JEeRSOx4uNtbqEs6RrM/fill/1418/1682/fp:0.5:0.5/1/aHR0cHM6Ly9tZWRp/YS5rdWtidWsucGwv/bWVkaWEvaW1hZ2Vz/LzIwMTkvMDMtMTgv/ZmV0dHVjaW5lMV90/aW5pZmllZF90aW5p/ZmllZC5qcGc.jpg',
 
-    tag: ["Vegan"],
+    tag: ['Pasta'],
   },
   {
-    name: "Zapiekanka",
+    name: 'Zapiekanka',
     image:
-      "https://images.aws.nestle.recipes/original/c6627deb1dc1c58e56beee298f1710fb_1500zapiekanka_ziemniaczana_z_mięsem_mielonym__50_copy.jpg",
+      'https://images.aws.nestle.recipes/original/c6627deb1dc1c58e56beee298f1710fb_1500zapiekanka_ziemniaczana_z_mięsem_mielonym__50_copy.jpg',
 
-    tag: ["Vegan"],
+    tag: ['Vegetarian'],
   },
   {
-    name: "Zupa krem",
+    name: 'Zupa krem',
     image:
-      "https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/zupa-krem-z-pieczonej-papryki-01.jpg",
-    tag: ["Vegan", "Soup", "Vegetarian"],
+      'https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/zupa-krem-z-pieczonej-papryki-01.jpg',
+    tag: ['Vegan', 'Soup', 'Vegetarian'],
   },
-];
+]
 
 function App() {
+  const [activeTag, setActiveTag] = useState('All')
+  const tags = ['All', 'Vegan', 'Vegetarian', 'Soup', 'Pasta']
+  const [dishes, setDishes] = useState([])
+
+  useEffect(() => {
+    const filteredDishes = TEMP_DATA.filter((dish) =>
+      activeTag === 'All' ? true : dish.tag.includes(activeTag)
+    )
+
+    setDishes(filteredDishes)
+  }, [activeTag])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -33,13 +47,18 @@ function App() {
         </div>
         <h1 className="text">Explore recipes</h1>
       </header>
+      <div style={{ display: 'flex' }}>
+        {tags.map((tag) => (
+          <TagMenu activeTag={activeTag} tagName={tag} onClick={setActiveTag} />
+        ))}
+      </div>
       <div className="cardsContainer">
-        {TEMP_DATA.map((food) => (
+        {dishes.map((food) => (
           <FoodCard name={food.name} image={food.image} tags={food.tag} />
         ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
