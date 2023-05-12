954cd959a38ecc3f99aec03c81723aae6e1a3f93
Karolina Dbaj <k.dbaj@outlook.com>
2023-05-12 21:32:59 +0200

    FoodCard style

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,33 +1,23 @@
-import React, { useEffect } from "react";
-import "./FoodCard.css";
-import Tag from "../Tag/Tag";
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
-</style>
+import React from 'react'
+import './FoodCard.css'
+import Tag from '../Tag/Tag'
 
-function FoodCard({image, name,tags}) {
-
-  
-
-    return(
-      <div className="card">
-        <div className="card-image">
-          <div className="image-container">
-            <img src={image} alt={name}/>
-          </div>
-          <div>
-             <h3 className="title">{name} </h3>
-            <div className="tagContainer">
-              {
-                tags.slice(0,3).map((tag) => {
-                  return <Tag tag={tag}/>
-                }
-                )}
-            </div>
-          </div>
+function FoodCard({ image, name, tags }) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <div className="image-container">
+          <img src={image} alt={name} />
+        </div>
+        <h3 className="title">{name} </h3>
+        <div className="tagContainer">
+          {tags.slice(0, 3).map((tag) => {
+            return <Tag tag={tag} />
+          })}
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
-export default FoodCard;
+export default FoodCard
