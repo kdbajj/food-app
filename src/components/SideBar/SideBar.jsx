1b204e0c88bcc8f538dc7022d42cc58adfc873d9
Karolina Dbaj <k.dbaj@outlook.com>
2023-06-15 13:36:00 +0200

    added items to sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,6 +3,8 @@ import * as FaIcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
 import './SideBar.css'
 import { SideBarData } from './SideBarData'
+import { IconContext } from 'react-icons'
+import { hover } from '@testing-library/user-event/dist/hover'
 
 function SideBar() {
   const [sidebar, setSidebar] = useState(false)
@@ -10,24 +12,26 @@ function SideBar() {
   const showSidebar = () => setSidebar(!sidebar)
   return (
     <>
-      <div className="container">
-        <FaIcons.FaBars className="nav-icon" onClick={showSidebar} />
+      <div className="navbar">
+        <div className="menu-bars">
+          <FaIcons.FaBars className="menu-icon" onClick={showSidebar} />
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <AiIcons.AiOutlineClose className="nav-icon" />
+            </li>
+            {SideBarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  {React.cloneElement(item.icon, { className: 'nav-icon' })}
+                  <span className="nav-icon">{item.title}</span>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
       </div>
-      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-        <ul className="nav-menu-items">
-          <li className="navbar-toggle">
-            {/* <AiIcons.AiOutlineClose className="nav-icon" /> */}
-          </li>
-          {SideBarData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                {React.cloneElement(item.icon, { className: 'nav-icon' })}
-                <span>{item.title}</span>
-              </li>
-            )
-          })}
-        </ul>
-      </nav>
     </>
   )
 }
